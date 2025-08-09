7bc4f4e73eab92554b547685ddef5d92ad815550
HudaifM <hudaifath.m@mountbox.in>
2025-08-09 14:19:48 +0530

    updated the cors

--- server.js
+++ server.js
@@ -5,7 +5,29 @@ require('dotenv').config();
 const path = require('path');
 
 const app = express();
-app.use(cors());
+const allowedOrigins = [
+  "https://fe-face-verification-app.onrender.com/", // your frontend in production
+  "http://localhost:3000/" // for local development
+];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl)
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true
+// }));
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}))
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
