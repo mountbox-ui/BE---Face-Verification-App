4826834ee871bb92e9b878c5ea8b817be1878cac
HudaifM <hudaifath.m@mountbox.in>
2025-08-10 00:19:59 +0530

    updated

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,20 @@ const User = require('../models/User');
 
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    if (username === 'user1' && password === 'password123') {
+    try {
+    // const user = await User.findOne({ username });
+    // console.log(user);
+    console.log(username, password);
+    if (username === 'testuser' && password === 'password123') {
+        // const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        res.json({ message: 'Login successful'});
+    }else {
         return res.status(400).json({ message: 'Invalid credentials' });
     }
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
-    res.json({ token });
+    } catch (error) {
+        // console.log('Login error:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
 });
 
 // For first time, create an admin user (remove in production)
--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.use('/api/verification', verificationRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(process.env.MONGO_URL,)
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
     .catch((err) => console.log(err));
