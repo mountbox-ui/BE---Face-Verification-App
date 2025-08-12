688a8ff99d6621734f37e71dbec8d7eefcc06fd9
HudaifM <hudaifath.m@mountbox.in>
2025-08-12 10:48:26 +0530

    add school faild issue

--- server.js
+++ server.js
@@ -22,16 +22,7 @@ const allowedOrigins = [
 
 // CORS configuration - Use only one CORS middleware
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, curl requests, or server-to-server)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*' ,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
